481b30b90c9b5125da1aec20af7f15076b8fee15
Christiaan <anotherhero@gmail.com>
2014-02-21 10:30:56 +0100

    Make more complete, should be everything needed for SMTP

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-exports.Request = function (request) {
+exports.Request = Request;
+exports.Response = Response;
+
+/**
+ *
+ * @param {IncomingMessage} request
+ * @constructor
+ */
+function Request(request) {
     var headers = request.headers;
+    this.secret = headers['x-auth-key'];
     this.sslSerial = headers['ssl-serial'];
     this.protocol = headers['auth-protocol'];
     this.user = headers['auth-user'];
@@ -8,22 +17,34 @@ exports.Request = function (request) {
     this.clienHost = headers['client-host'];
     this.host = headers['host'];
     this.loginAttempt = headers['auth-login-attempt'];
-};
+}
 
-exports.Response = function (response) {
+/**
+ * @param {ServerResponse} response
+ * @constructor
+ */
+function Response(response) {
     this.response = response;
+}
+
+Response.prototype.okForwardTo = function (server, port) {
+    this.response.statusCode = 200;
+    this.response.setHeader('Auth-Status', 'OK');
+    this.response.setHeader('Auth-Server', server);
+    this.response.setHeader('Auth-Port', port);
 };
 
-exports.Response.prototype = {
-    okForwardTo: function (server, port) {
-        this.response.statusCode = 200;
-        this.response.setHeader('Auth-Status', 'OK');
-        this.response.setHeader('Auth-Server', server);
-        this.response.setHeader('Auth-Port', port);
-    },
-    tryAgain: function (sec) {
-        this.response.statusCode = 200;
-        this.response.setHeader('Auth-Status', 'Temporary server problem, try again later');
-        this.response.setHeader('Auth-Wait', '' + sec);
+Response.prototype.invalidLogin = function (later) {
+    this.response.statusCode = 200;
+    this.response.setHeader('Auth-Status', 'Invalid login or password');
+    if (later) {
+        this.response.setHeader('Auth-Wait', '' + later);
     }
+};
+
+Response.prototype.serverProblem = function () {
+    this.response.statusCode = 200;
+    this.response.setHeader('Auth-Status', 'Temporary server problem, try again later');
+    this.response.setHeader('Auth-Error-Code', '451 4.3.0');
+    this.response.setHeader('Auth-Wait', '3');
 };
